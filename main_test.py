833abb99fd40f6bbce0eb14053d6387954e7ec38
Azamat Abdybekov <azamat_abdybekov@epam.com>
2025-02-23 18:53:36 +0600

    fix_5

--- main_test.py
+++ main_test.py
@@ -1,11 +1,12 @@
 import unittest
 import json
-from fastapi.testclient import TestClient  # Ensure this is imported correctly
-from main import app  # Import the app from your fastapi setup
+from fastapi.testclient import TestClient
+from main import app
+
 
 class TestApp(unittest.TestCase):
     def setUp(self):
-        self.client = TestClient(app)  # This sets up the client using your FastAPI app
+        self.client = TestClient(app)
         with open("data.json", "r") as f:
             self.data = json.load(f)
 
@@ -15,17 +16,15 @@ class TestApp(unittest.TestCase):
         self.assertEqual(response.json(), self.data)
 
     def test_read_data_by_guid(self):
-        if len(self.data) > 0:
-            guid = self.data[0]["guid"]
-            response = self.client.get(f"/{guid}")
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(response.json(), self.data[0])
-        else:
-            self.skipTest("No data available to test")
+        guid = self.data[0]["guid"]
+        response = self.client.get(f"/{guid}")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), self.data[0])
 
     def test_read_data_by_invalid_guid(self):
         response = self.client.get("/invalid-guid")
         self.assertEqual(response.status_code, 404)
+
 
 if __name__ == "__main__":
     unittest.main()
